397316b93ccb1e5d2d2f2decfde591956fa5b4b7
majaengber02 <maja.engberg@gmail.com>
2025-10-17 15:22:34 +0200

    Fetching recipes from API

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ function getRecipes() {
 
   // Calling the API
   const xhttp = new XMLHttpRequest();
-  xhttp.open("GET", "response.json", false);
+  xhttp.open("GET", "https://api.spoonacular.com/recipes/random?apiKey=24c0ad763b794214b472e5a5e70fac09&number=20&includeNutrition=false", false);
   xhttp.send();
 
   // Checking for response
